bde6d089fb31058de45570205b6543eec4f66315
GAsishPatro <gasishpatro03@gmail.com>
2024-02-11 18:48:55 +0530

    minor change

--- app/src/main/java/com/personal/myapplication/HomeActivity.kt
+++ app/src/main/java/com/personal/myapplication/HomeActivity.kt
@@ -5,11 +5,10 @@ import android.content.Intent
 import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import com.personal.myapplication.databinding.ActivityMainBinding
+
 
 class HomeActivity : AppCompatActivity() {
 
